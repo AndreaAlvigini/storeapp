7560f49cb94d9b7e968989299e88ab39fb6e18bf
AndreaAlvigini <andreaalvigini@gmail.com>
2023-09-07 12:58:32 +0200

    Archetype con servlet index.jsp funzionante

---
+++ full-webapp/src/main/java/AcquistoServlet.java
@@ -0,0 +1,35 @@
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager; // Importa DriverManager da java.sql
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AcquistoServlet extends HttpServlet {
+    private AcquistoDAO acquistoDAO;
+
+    public void init() {
+        String url = "jdbc:sqlite:database.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Errore durante la connessione al database.", e);
+        }
+        acquistoDAO = new AcquistoDAO(conn);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Acquisto> acquisti = acquistoDAO.getAllAcquisti();
+        request.setAttribute("acquisti", acquisti);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("acquisti.jsp");
+        dispatcher.forward(request, response);
+    }
+}
---
+++ full-webapp/src/main/java/ProdottoServlet.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager; // Importa DriverManager da java.sql
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ProdottoServlet extends HttpServlet {
+    private ProdottoDAO prodottoDAO;
+
+    public void init() {
+        String url = "jdbc:sqlite:database.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Errore durante la connessione al database.", e);
+        }
+        prodottoDAO = new ProdottoDAO(conn);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Prodotto> prodotti = prodottoDAO.getAllProdotti();
+        request.setAttribute("prodotti", prodotti);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("prodotti.jsp");
+        dispatcher.forward(request, response);
+    }
+}
